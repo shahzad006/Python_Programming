e0187abcd7c25aa83e06e5e83be29db27c29a7fb
Muhammad Shahzad <160237343+shahzad006@users.noreply.github.com>
2025-07-31 23:49:34 +0500

    first comment

---
+++ lectures/04_operators/README.md
@@ -0,0 +1,33 @@
+# Python Operators
+
+## 🧮 What Are Python Operators?
+Python operators are special symbols or words that perform operations on variables and values. Think of them as tools to do math, compare values, or combine logic.
+
+## 🔹 Types of Python Operators
+1. Arithmetic
+	Do math (add, subtract, multiply, etc.)
+    	`+`, `-`, `*`
+
+2. Assignment
+    Assign values to variables
+    	`=`, `+=`, `-=`
+
+3. Comparison 
+    Compare values (True/False)
+    	`==`, `!=`, `>` , `<=`
+
+4. Logical
+    Combine conditions (True/False logic)
+    	`and`, `or`, `not`
+
+5. Bitwise
+    Work on bits (0s and 1s)
+    	`&`, `
+
+6. Membership
+	Check if a value is in a sequence
+    	`in`, `not in`
+
+7. Identity
+	Check if two variables are the same object
+    	`is`, `is not`
---
+++ lectures/04_operators/main.py
@@ -0,0 +1,83 @@
+
+#todo ----------------------------- Python Operators ----------------------------- #
+
+#! ---------------------------------------------------------------------------- #
+#?                            1. Arithmetic Operators                           #
+#! ---------------------------------------------------------------------------- #
+
+num_1:int = 5
+num_2:int = 2
+
+
+print(num_1 + num_2)            # Addition
+print(num_1 - num_2)            # Subtraction
+print(num_1 * num_2)            # Multiply
+print(num_1 / num_2)            # Divide
+print(num_1 // num_2)           # Floor Divide
+print(num_1 % num_2)            # Modulus (Remainder)Divide
+print(num_1 ** num_2)           #Power
+
+
+
+#! ---------------------------------------------------------------------------- #
+#?                            2. Assignment Operators                           #
+#! ---------------------------------------------------------------------------- #
+
+num_1:int = 7
+num_2:int = 3
+
+num_1 += num_2
+print(num_1)            # Addition
+
+num_1 -= num_2
+print(num_1)            # Subtraction
+
+num_1 *= num_2
+print(num_1)            # Multiply
+
+num_1 /= num_2
+print(num_1)            # Divide
+
+num_1 //= num_2
+print(num_1)            # Floor Divide
+
+num_1 %= num_2
+print(num_1)            # Modulus (Remainder)Divide
+
+num_1 **= num_2
+print(num_1)            #Power
+
+
+
+
+#! ---------------------------------------------------------------------------- #
+#?                            3. Comparison Operators                           #
+#! ---------------------------------------------------------------------------- #
+
+
+num_1 = 7
+num_2 = 4
+
+print(num_1 == num_2)   # Equal
+print(num_1 != num_2)   # Not Equal
+print(num_1 > num_2)    # Greater
+print(num_1 < num_2)    # Less
+print(num_1 >= num_2)   # Greater Equal
+print(num_1 <= num_2)   # Less Equal
+
+
+
+#! ---------------------------------------------------------------------------- #
+#?                             4. Logical Operators                             #
+#! ---------------------------------------------------------------------------- #
+
+num_1 = 7
+num_2 = 4
+
+print(num_1 > 5 and num_2 < 5)   # Logical AND
+print(num_1 > 5 or num_2 < 5)    # Logical OR
+print(not(num_1 > 5))             # Logical NOT
+
+
+
+
